f23719cea6897664ce292905ede3069252d1d074
russ-davey <russell.j.davey@gmail.com>
2021-12-13 12:03:51 +0000

    add error handling

--- api/files.go
+++ api/files.go
@@ -21,7 +21,24 @@ func CreateFileUploadStartedHandler(creatorFunc files.CreateUploadStartedEntry) 
 		m.LastModified = time.Now()
 		m.State = "CREATED"
 
-		creatorFunc(req.Context(), m)
+		encoder := json.NewEncoder(w)
+
+		type jsonError struct {
+			Code string `json:"code"`
+			Description string `json:"description"`
+		}
+
+		type JsonErrors struct {
+			Error []jsonError `json:"errors"`
+		}
+
+		err = creatorFunc(req.Context(), m)
+		if err != nil {
+			errs := JsonErrors{Error: []jsonError{{Description: err.Error(), Code: "DatabaseError"}}}
+			w.WriteHeader(http.StatusInternalServerError)
+			encoder.Encode(&errs)
+			return
+		}
 
 		w.WriteHeader(http.StatusCreated)
 	}
--- api/files_test.go
+++ api/files_test.go
@@ -1,9 +1,31 @@
 package api_test
 
 import (
+	"bytes"
+	"context"
+	"errors"
+	"github.com/ONSdigital/dp-files-api/api"
+	"github.com/ONSdigital/dp-files-api/files"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
-func TestFileMetaDataCreationSuccessful(t *testing.T) {
+func TestFileMetaDataCreationUnsuccessful(t *testing.T) {
+	rec := httptest.NewRecorder()
+	body := bytes.NewBufferString(`{"test": "test"}`)
+	req := httptest.NewRequest(http.MethodPost, "/v1/files", body)
 
+	errFunc := func (ctx context.Context, metaData files.MetaData) error {
+		return errors.New("it's all gone very wrong")
+	}
+
+	h := api.CreateFileUploadStartedHandler(errFunc)
+	h.ServeHTTP(rec, req)
+
+	assert.Equal(t, http.StatusInternalServerError, rec.Code)
+	response, _ := ioutil.ReadAll(rec.Body)
+	assert.Contains(t, string(response), "it's all gone very wrong")
 }
