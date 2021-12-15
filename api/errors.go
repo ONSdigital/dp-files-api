c4287a02627dcc00b06806e39df22742b1b47dff
Jon Acker <jon.acker@yoyo.org>
2021-12-15 10:16:23 +0000

    move error handling to separate file

---
+++ api/errors.go
@@ -0,0 +1,33 @@
+package api
+
+import (
+	"encoding/json"
+	"github.com/ONSdigital/dp-files-api/files"
+	"net/http"
+)
+
+type jsonError struct {
+	Code        string `json:"code"`
+	Description string `json:"description"`
+}
+
+type jsonErrors struct {
+	Error []jsonError `json:"errors"`
+}
+
+func handleError(w http.ResponseWriter, err error) {
+	switch err {
+	case files.ErrDuplicateFile:
+		writeError(w, err, "DuplicateFileError", http.StatusBadRequest)
+	default:
+		writeError(w, err, "DatabaseError", http.StatusInternalServerError)
+	}
+}
+
+func writeError(w http.ResponseWriter, err error, code string, httpCode int) {
+	encoder := json.NewEncoder(w)
+
+	errs := jsonErrors{Error: []jsonError{{Description: err.Error(), Code: code}}}
+	w.WriteHeader(httpCode)
+	encoder.Encode(&errs)
+}
--- api/files.go
+++ api/files.go
@@ -6,15 +6,6 @@ import (
 	"net/http"
 )
 
-type jsonError struct {
-	Code        string `json:"code"`
-	Description string `json:"description"`
-}
-
-type jsonErrors struct {
-	Error []jsonError `json:"errors"`
-}
-
 func CreateFileUploadStartedHandler(creatorFunc files.CreateUploadStartedEntry) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 
@@ -33,21 +24,4 @@ func CreateFileUploadStartedHandler(creatorFunc files.CreateUploadStartedEntry) 
 
 		w.WriteHeader(http.StatusCreated)
 	}
-}
-
-func handleError(w http.ResponseWriter, err error) {
-	switch err {
-	case files.ErrDuplicateFile:
-		writeError(w, err, "DuplicateFileError", http.StatusBadRequest)
-	default:
-		writeError(w, err, "DatabaseError", http.StatusInternalServerError)
-	}
-}
-
-func writeError(w http.ResponseWriter, err error, code string, httpCode int) {
-	encoder := json.NewEncoder(w)
-
-	errs := jsonErrors{Error: []jsonError{{Description: err.Error(), Code: code}}}
-	w.WriteHeader(httpCode)
-	encoder.Encode(&errs)
 }
