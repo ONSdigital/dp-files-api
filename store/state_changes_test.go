6b779126bbd5035c007cff41af039754020426e4
aryan-patel <aryanpatel1771@gmail.com>
2025-04-02 14:58:57 +0100

    add extra unit test for bundle

--- store/state_changes_test.go
+++ store/state_changes_test.go
@@ -124,7 +124,7 @@ func (suite *StoreSuite) TestRegisterFileUploadWhenBundleAlreadyPublished() {
 	suite.ErrorIs(err, store.ErrBundleAlreadyPublished)
 }
 
-func (suite *StoreSuite) TestRegisterFileUploadWhenFilePathAlreadyExists() {
+func (suite *StoreSuite) TestRegisterFileUploadWhenFilePathAlreadyExistsCollection() {
 	suite.logInterceptor.Start()
 	defer suite.logInterceptor.Stop()
 
@@ -143,6 +143,33 @@ func (suite *StoreSuite) TestRegisterFileUploadWhenFilePathAlreadyExists() {
 
 	cfg, _ := config.Get()
 	subject := store.NewStore(&alwaysFindsExistingCollection, &emptyCollection, nil, &suite.defaultKafkaProducer, suite.defaultClock, nil, cfg)
+	err := subject.RegisterFileUpload(suite.defaultContext, metadata)
+
+	logEvent := suite.logInterceptor.GetLogEvent()
+
+	suite.Equal("File upload already registered: skipping registration of file metadata", logEvent)
+	suite.NoError(err)
+}
+
+func (suite *StoreSuite) TestRegisterFileUploadWhenFilePathAlreadyExistsBundle() {
+	suite.logInterceptor.Start()
+	defer suite.logInterceptor.Stop()
+
+	metadata := suite.generateBundleMetadata(suite.defaultBundleID)
+	metadata.State = store.StateUploaded
+	metadataBytes, _ := bson.Marshal(metadata)
+
+	alwaysFindsExistingCollection := mock.MongoCollectionMock{
+		FindOneFunc: CollectionFindOneSetsResultAndReturnsNil(metadataBytes),
+	}
+	emptyCollection := mock.MongoCollectionMock{
+		FindOneFunc: func(ctx context.Context, filter, result interface{}, opts ...mongodriver.FindOption) error {
+			return mongodriver.ErrNoDocumentFound
+		},
+	}
+
+	cfg, _ := config.Get()
+	subject := store.NewStore(&alwaysFindsExistingCollection, nil, &emptyCollection, &suite.defaultKafkaProducer, suite.defaultClock, nil, cfg)
 	err := subject.RegisterFileUpload(suite.defaultContext, metadata)
 
 	logEvent := suite.logInterceptor.GetLogEvent()
