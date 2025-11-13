31a8884be5e671b086a678402ce697ae8a2bd050
aryan-patel <aryanpatel1771@gmail.com>
2025-11-13 16:08:13 +0000

    create apierror struct for returning errors

---
+++ sdk/errors.go
@@ -0,0 +1,33 @@
+package sdk
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/ONSdigital/dp-files-api/api"
+)
+
+// APIError represents an error returned by the files API
+type APIError struct {
+	StatusCode int
+	Errors     *api.JsonErrors
+}
+
+// Error implements the error interface for APIError
+func (e *APIError) Error() string {
+	errorMessage := fmt.Sprintf("API error: status code %d", e.StatusCode)
+
+	if e.Errors == nil || len(e.Errors.Error) == 0 {
+		return errorMessage
+	}
+
+	for _, err := range e.Errors.Error {
+		errorMessage += fmt.Sprintf("\n  - code: %s, description: %s", err.Code, err.Description)
+	}
+	return errorMessage
+}
+
+// List of errors that can be returned by the SDK
+var (
+	ErrMissingResponseBody = errors.New("missing response body")
+)
